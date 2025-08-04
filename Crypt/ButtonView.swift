ce1e1c1033d4b6f9ebad018b549c46abfd50b8a5
metelykx <ivaschenkoodenys@gmail.com>
2025-08-04 14:11:55 +0300

    new file as ButtonView

---
+++ Crypt/ButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonView.swift
+//  Crypt
+//
+//  Created by Denis Ivaschenko on 04.08.2025.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    
+    let size: CGSize
+    var body: some View {
+        Button {
+            print("Sign Up tapped")
+        } label: {
+            Text("Sign Up")
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, size.height * 0.03)
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(12)
+        .padding(.horizontal, size.width * 0.2)
+        .padding(.top, size.height * 0.03)
+    }
+}
