e95b17ed8b69078ea2fd6ea698720d761854a1d5
metelykx <ivaschenkoodenys@gmail.com>
2025-08-04 18:18:00 +0300

    circles

--- Crypt/StartView.swift
+++ Crypt/StartView.swift
@@ -11,8 +11,10 @@ struct StartView: View {
     var body: some View {
         Text("alala")
         ZStack {
+            
             GeometryReader { geometry in
-                
+                Circle1View(size: geometry.size)
+                Circle2View(size: geometry.size)
             }
         }
     }
