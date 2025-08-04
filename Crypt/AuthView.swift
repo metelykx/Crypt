2d94366708555e1164c437862e690e1fe472ef1c
metelykx <ivaschenkoodenys@gmail.com>
2025-08-04 13:20:22 +0300

    del

--- Crypt/AuthView.swift
+++ Crypt/AuthView.swift
@@ -77,20 +77,3 @@ struct AuthView: View {
     AuthView()
 }
 
-struct HeaderView: View {
-    let sizes: CGSize
-    var body: some View {
-        HStack {
-            Text("Welcome to Crypt")
-                .font(.title)
-                .bold()
-                .fontWidth(.compressed)
-                .foregroundStyle(.primary)
-            
-            Image(systemName: "bitcoinsign.bank.building.fill")
-                .resizable()
-                .frame(width: sizes.width / 10, height: sizes.height / 20)
-                .foregroundStyle(.primary)
-        }
-    }
-}
