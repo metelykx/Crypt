17c4944fe4a101cb94fe9e811741dcdb91ba3e61
metelykx <ivaschenkoodenys@gmail.com>
2025-08-04 18:28:35 +0300

    VStack

--- Crypt/StartView.swift
+++ Crypt/StartView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct StartView: View {
+    
     @Environment(\.colorScheme) private var colorScheme
     private var textColor: Color {
         colorScheme == .dark ? .white : .black
@@ -18,18 +19,19 @@ struct StartView: View {
             GeometryReader { geometry in
                 Circle1View(size: geometry.size)
                 Circle2View(size: geometry.size)
-                
-                HStack {
-                    Text("Crypto")
-                        .font(.title)
-                        .bold()
-                        .fontWidth(.compressed)
-                        .foregroundStyle(textColor)
-                    
-                    Image(systemName: "bitcoinsign.bank.building.fill")
-                        .resizable()
-                        .frame(width: geometry.size.width / 10, height: geometry.size.height / 20)
-                        .foregroundStyle(.yellow)
+                VStack {
+                    HStack {
+                        Text("Crypto")
+                            .font(.title)
+                            .bold()
+                            .fontWidth(.compressed)
+                            .foregroundStyle(textColor)
+                        
+                        Image(systemName: "bitcoinsign.bank.building.fill")
+                            .resizable()
+                            .frame(width: geometry.size.width / 10, height: geometry.size.height / 20)
+                            .foregroundStyle(.yellow)
+                    }
                 }
             }
         }
