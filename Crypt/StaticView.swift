f993b32c7521e8e032b63862b824d3a34891cc0e
metelykx <ivaschenkoodenys@gmail.com>
2025-08-06 14:56:29 +0300

    new struct

--- Crypt/StaticView.swift
+++ Crypt/StaticView.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Denis Ivaschenko on 06.08.2025.
 //
+import SwiftUI
 
+struct StaticView: View {
+    var body: some View {
+        Text("dede")
+    }
+}
+
+
+#Preview {
+    StaticView()
+}
